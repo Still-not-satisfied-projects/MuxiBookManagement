c7d66896c7467f4fc85b032ace2bdfb5cca5b863
neo1218 <neo1218@yeah.net>
2015-08-27 15:34:55 +0800

    细节处理、添加权限

--- app/__init__.py
+++ app/__init__.py
@@ -13,7 +13,8 @@ from flask.ext.login import LoginManager
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "I hate flask!"
 # app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///path/to/data.sqlite"  # 系统相应替换
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////path/to/data.sqlite"  # 系统相应替换
+# app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////path/to/data.sqlite"  # 系统相应替换
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////Users/apple/www/project/MuxiBookManagement/data.sqlite"  # 系统相应替换
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 app.config['WHOOSH_BASE'] = "search.db"
 app.config['MAX_SEARCH_RESULTS'] = 5  # 最大加载5个搜索结果
